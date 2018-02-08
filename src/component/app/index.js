da4e65026370191814c655d0dbfc7fe38b5de7c6
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-02-08 12:37:26 -0800

    Styling landing page and its components

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import MetaTags from 'react-meta-tags';
 
 import { AppBar } from 'material-ui';
 
@@ -22,9 +23,11 @@ class App extends Component {
       <Fragment>
        
         { /* <NavWrapper /> */}
-
         <BrowserRouter>
           <div className="app">
+            <MetaTags>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </MetaTags>
             <Fragment>
               <Route path="*" component={AuthRedirect} />
               <Route exact path="/" component={Landing} />
