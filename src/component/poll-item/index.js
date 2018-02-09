1dee886f00ca3747b262bfc2504551bed271b333
Robert Reed <robert.mc.reed@gmail.com>
2018-02-08 22:19:17 -0800

    polls appear newest first, voter fraud protection

--- src/component/poll-item/index.js
+++ src/component/poll-item/index.js
@@ -1,11 +1,11 @@
 import { connect } from 'react-redux';
 import React, { Component, Fragment } from 'react';
 
+import './_poll-item.scss';
 import Meter from '../meter';
 import Voting from '../voting';
 import { log } from '../../lib/util';
 
-import './_poll-item.scss';
 
 class PollItem extends Component {
   handleVoteSubmit = (vote) => {
@@ -32,6 +32,9 @@ class PollItem extends Component {
       pollId,
     } = poll;
 
+    const resultsArray = Object.keys(results).map(key => results[key]);
+    const totalVotes = resultsArray.reduce((a, b) => a + b);
+
     const votingJSX = !owner ? 
       <Voting handleVote={this.handleVoteSubmit} pollId={pollId} /> :
       null;
@@ -39,8 +42,9 @@ class PollItem extends Component {
     return (
       <Fragment>
         <h2>{question}:</h2>
-        {votingJSX}         
-        <Meter results={results} />
+        <div>Votes: {totalVotes}</div>
+        {votingJSX}     
+        <Meter resultsArray={resultsArray} totalVotes={totalVotes} />
       </Fragment>
     );
   }
