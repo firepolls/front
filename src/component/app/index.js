205e46ff17a2295991156eef5fc373ec7d236f95
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-02-08 16:18:24 -0800

    Have a mostly functional header

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -22,7 +22,7 @@ class App extends Component {
     return (
       <Fragment>
        
-        { /* <NavWrapper /> */}
+        <NavWrapper />
         <BrowserRouter>
           <div className="app">
             <MetaTags>
--- src/component/material-ui/nav-wrapper/index.js
+++ src/component/material-ui/nav-wrapper/index.js
@@ -1,11 +1,15 @@
 import React, { Component, Fragment } from 'react';
-import { AppBar, Drawer, MenuItem, RaisedButton } from 'material-ui';
+import { AppBar, Drawer, MenuItem, RaisedButton, Popover, Menu, Paper } from 'material-ui';
+import AuthForm from '../../auth-form';
 import mastHead from '../nav-wrapper/navstyling';
 
 
 class NavWrapper extends Component {
     state = {
       open: false,
+      signingUp: false,
+      loggingIn: false,
+      anchorEl: null,
     }
 
     handleToggle = () => {
@@ -13,27 +17,96 @@ class NavWrapper extends Component {
     }
 
     render() {
-      const { classes } = this.props;
-      return (
-        <div>
-          <AppBar 
-            title="Fire Polls" 
-            onLeftIconButtonClick={this.handleToggle}
-            zDepth={3}
-            iconStyleLeft={mastHead}
-          />
-          <Drawer 
-            docked={false}
-            width={200}
-            open={this.state.open}
-            onRequestChange={(open) => this.setState({ open })}
-            zDepth={2}
+      const { 
+        socket,
+        signup,
+        login,
+        logout,
+      } = this.props;
+
+      const signupLoginJSX =
+        (
+          <Menu 
+            style={{
+              width: '10em',
+            }}
           >
-            <h1> Fire Polls </h1>
-            <MenuItem>Sessions</MenuItem>
-            <MenuItem>Polls</MenuItem>
-          </Drawer>
-        </div>
+      
+            <MenuItem 
+              primaryText="Signup"
+              onClick={() =>
+                this.setState({
+                  signingUp: true,
+                  loggingIn: false,
+                  open: false,
+                })}
+            />
+            <MenuItem 
+              primaryText="Login"
+              onClick={() =>
+                this.setState({
+                  loggingIn: true,
+                  signingUp: false,
+                  open: false,
+                })}
+            />
+          </Menu>
+        );
+
+      const logoutJSX =
+        (
+          <div>
+            <ul className="nav-items">
+              <li>
+                <RaisedButton onClick={() => logout(socket.socket)}>Logout</RaisedButton>
+              </li>
+            </ul>
+          </div>
+        );
+
+      return (
+        <Paper
+          style={{
+            marginLeft: 'auto',
+            textAlign: 'right',
+          }}
+        >
+
+          <RaisedButton
+            label="Signup/Login"
+            onClick={(event) => this.setState({
+              anchorEl: event.target,
+              open: true,
+            })}
+          />
+          <Popover
+            style={{
+              marginTop: '1em',
+            }}
+            targetOrigin={ 
+              {
+                horizontal: 'right',
+                vertical: 'top',
+              }
+            }
+            anchorOrigin={
+              {
+                horizontal: 'right',
+                vertical: 'bottom',
+              }
+            }
+            anchorEl={this.state.anchorEl}
+            open={this.state.open}
+          >
+            {this.props.loggedIn ? logoutJSX : signupLoginJSX}
+         
+          </Popover>
+
+          <div className="form-container">
+            {this.state.signingUp && !this.props.loggedIn ? <AuthForm type="signup" onComplete={signup} /> : null}
+            {this.state.loggingIn ? <AuthForm type="login" onComplete={login} /> : null}
+          </div>
+        </Paper>
       );
     }
 }
