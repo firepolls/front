74a1ee6b1c7569f5455fe52c88c2dd6dbb94af01
Predrag Josifovic <pjosifovic@yahoo.com>
2018-02-09 09:00:32 -0800

    adding imgur image links

--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -110,7 +110,7 @@ class Landing extends Component {
 
           <div className="grid">
             <div className="feature-left">
-              <img src="../../../static/images/socket-io.png" alt="Real Time" />
+              <img src="https://i.imgur.com/wfvLvuq.png" alt="Real Time" />
               <h2>Real-Time</h2>
               <p className="feature-desc">
                 Enables bi-directional, event-based communication and 
@@ -118,14 +118,14 @@ class Landing extends Component {
               </p>
             </div>
             <div className="feature-center">
-              <img src="../../../static/images/analytics.png" alt="Analytics" />
+              <img src="https://i.imgur.com/RLqWOTH.png" alt="Analytics" />
               <h2>Analytics</h2>
               <p className="feature-desc">
                 Push data to clients and visualize results in real time.
               </p>
             </div>
             <div className="feature-right">
-              <img src="../../../static/images/stars.png" alt="Anonymous Voting" />
+              <img src="https://i.imgur.com/DwpqLpv.png" alt="Anonymous Voting" />
               <h2>Anonymous Voting</h2>
               <p className="feature-desc">
                 Keep track of how many people have responded to a poll while keeping votes anonymous.
