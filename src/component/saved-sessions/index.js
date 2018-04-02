0a1684acd373efb9f278fc3cf0479567c7a1a170
SethDonohue <donohue.seth@gmail.com>
2018-04-01 19:00:22 -0700

    added instructions for nore saverooms and styling for savedsession component

--- src/component/saved-sessions/index.js
+++ src/component/saved-sessions/index.js
@@ -1,20 +1,37 @@
+import { Paper } from 'material-ui';
 import { connect } from 'react-redux';
 import React, { Component, Fragment } from 'react';
 
 import Room from '../room';
+import './_saved-sessions.scss';
 
 class SavedSessions extends Component {
   render() {
+    const instructionsText = (
+      <Fragment>
+        <p>No Saved Rooms...</p>
+        <p><strong>Note:</strong> Create a room with at least one Poll and Save it for it to show here.
+        </p>
+      </Fragment>);
+
     return (
       <section className="saved-sessions">
         <ul>
-          {
-            this.props.savedRooms
-              .map(savedRoom => (
-                <li key={savedRoom._id} >
-                  <Room savedRoom={savedRoom} />
-                </li>
-              ))
+          { this.props.savedRooms.length ?
+            (
+              this.props.savedRooms
+                .map(savedRoom => (
+                  <li key={savedRoom._id} >
+                    <Room savedRoom={savedRoom} />
+                  </li>
+                ))
+            ) : (
+              <Paper
+                zDepth={4}
+                className="instruction"
+              >
+                {instructionsText}
+              </Paper>)
           }
         </ul>
       </section>
