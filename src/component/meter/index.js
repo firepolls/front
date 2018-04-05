ec5ca3da7ce935abcbd1777bcd7b5f84f6e6aa0e
Robert Reed <robert.mc.reed@gmail.com>
2018-04-05 01:36:47 -0700

    sexy new bar colors

--- src/component/meter/index.js
+++ src/component/meter/index.js
@@ -17,10 +17,10 @@ class Meter extends Component {
     const percentages = resultsArray.map(result => result / divisor);
 
     const svgProperties = [ 
-      { width: percentages[0], color: 'blue', star: '★' },
-      { width: percentages[1], color: 'green', star: '★★' },
-      { width: percentages[2], color: 'red', star: '★★★' },
-      { width: percentages[3], color: 'yellow', star: '★★★★' },
+      { width: percentages[0], color: '#F5839B', star: '★' },
+      { width: percentages[1], color: '#EC0C90', star: '★★' },
+      { width: percentages[2], color: '#09AFEF', star: '★★★' },
+      { width: percentages[3], color: '#0EF207', star: '★★★★' },
     ];
 
     const meterJSX = svgProperties.map((properties, index) => ( 
